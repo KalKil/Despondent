5dd25dc83eb0ac081fd50f9e03390ce741b9fa56
Kalyn <kwilliams3@brooklynsteamcenter.org>
2023-03-23 10:54:12 -0400

    added more code

--- main.py
+++ main.py
@@ -5,5 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    print("despondent_testing_site")
     return render_template("home.html.jinja")
+
+if __name__ == '__main__':
+    app.run(debug=True)
