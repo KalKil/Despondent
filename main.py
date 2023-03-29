469415db39d2f8c481b2acb1e368665ee37e76e1
Kalyn <kwilliams3@brooklynsteamcenter.org>
2023-03-29 10:55:37 -0400

    ......this is truely Despondent....

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
+
 import pymysql
 import pymysql.cursors
-
 
 app = Flask(__name__)
 
@@ -35,4 +35,23 @@ def post_feed():
     "feed.html.jinja",
     posts=results
 )
+
+@app.route('/sign-in')
+def sign_in():
+    return render_template("sign_in.html.jinja")
+    
+@app.route('/sign-up', methods=['POST', 'GET'])
+def sign_up():
+    if request.method == 'POST':
+        # Handle signup
+        cursor = connection.cursor()
+
+        cursor.execute("""
+            INSERT INTO `user` (`username`, `email`, `display_name`, `pasword`, `bio`, `photo`)
+            VALUES(%s, %s, %s, %s, %s, %s,)
+        """, ())
+
+        return request.form
+    elif request.method == 'GET':
+        return render_template("sign_up.html.jinja")
 
