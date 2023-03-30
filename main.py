6b21306511212283f72dfed71df4e7c869d0c8ef
Kalyn <kwilliams3@brooklynsteamcenter.org>
2023-03-30 10:54:18 -0400

    done and done

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+
+from flask_login import LoginManager
 
 import pymysql
 import pymysql.cursors
 
+login_manager = LoginManager()
+
 app = Flask(__name__)
+login_manager.init_app(app)
+
+class User:
+    def __init__(self, id, username, banned):
+        self.is_authenticated = True
+        self.is_anonymous = False
+        self.is_active = not banned
+
+        self.username = username
+        self.id = id
+
+    def get_rid(self):
+        return str(self.id)
+
+
 
 @app.route("/")
 def index():
@@ -23,7 +42,20 @@ connection = pymysql.connect(
     autocommit=True
 )
 
-app.route('/feed')
+@login_manager.user_loader
+def user_loader(user_id):
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * from `users` WHERE `id` = " + user_id)
+
+    result = cursor.fetchone()
+
+    if result is None:
+        return None
+    
+    return User(result['id'], result['username'], result['banned'])
+
+@app.route('/feed')
 def post_feed():
 
     cursor = connection.cursor()
@@ -46,12 +78,24 @@ def sign_up():
         # Handle signup
         cursor = connection.cursor()
 
+        photo = request.files ['photo']
+
+        file_name = photo.filename # my_photo.jpg
+
+        file_extension = file_name.split('.')[-1]
+
+        if file_extension in ['jpg','jpeg', 'png', 'gif']:
+            photo.save('media/users/' + file_name)
+        else:
+            raise Exception('Invalid file type') 
+
         cursor.execute("""
-            INSERT INTO `user` (`username`, `email`, `display_name`, `pasword`, `bio`, `photo`)
+            INSERT INTO `user` (`username`, `email`, `display_name`, `password`, `bio`, `photo`)
             VALUES(%s, %s, %s, %s, %s, %s,)
-        """, ())
+        """, (request.form['username'], request.form['email'], request.form['display_name'], request.form ['password'], request.form ['bio'], file_name))
 
         return request.form
     elif request.method == 'GET':
         return render_template("sign_up.html.jinja")
+
 
