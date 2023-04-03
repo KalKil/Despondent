d404255a3bf3c6bdbd64cdfe904ae3e6daab5718
Kalyn <kwilliams3@brooklynsteamcenter.org>
2023-04-03 10:55:02 -0400

    added some stuff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect
 
-from flask_login import LoginManager
+from flask_login import LoginManager, login_required , login_user, current_user, logout_user                  
 
 import pymysql
 import pymysql.cursors
@@ -9,6 +9,8 @@ login_manager = LoginManager()
 
 app = Flask(__name__)
 login_manager.init_app(app)
+
+app.config['SECRET_KEY'] = 'something_random'
 
 class User:
     def __init__(self, id, username, banned):
@@ -56,27 +58,50 @@ def user_loader(user_id):
     return User(result['id'], result['username'], result['banned'])
 
 @app.route('/feed')
+@login_required
 def post_feed():
 
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM `post` ORDER BY `timestamp`")
+
+    cursor.execute("SELECT * FROM `post` JOIN `Users` ON `Posts`.`user_id` = `Users`. `id` Order by `date` DESC;")
 
     results = cursor.fetchall()
 
-    return render_template(
-    "feed.html.jinja",
-    posts=results
-)
+    return render_template("feed.html.jinja", posts = results)
 
-@app.route('/sign-in')
+@app.route('/sign-out')
+def sign_out():
+    logout_user()
+
+    return redirect('/sign-in')
+
+@app.route('/sign-in', methods=['POST', 'GET'])
 def sign_in():
-    return render_template("sign_in.html.jinja")
+     if current_user.is_authenticated:
+         return redirect('/feed')
+     if request.method == 'POST':
+        return render_template("sign_in.html.jinja")
     
 @app.route('/sign-up', methods=['POST', 'GET'])
 def sign_up():
+    if current_user.is_authenticated:
+      return redirect('/feed')
     if request.method == 'POST':
-        # Handle signup
         cursor = connection.cursor()
+        
+        cursor.execute(f"SELECT * FROM `users` WHERE `username` = '{request.form['username']}'")
+
+        result = cursor.fetchone()
+
+        if result is None:
+            return render_template("sign_.html.jinja")
+        
+        if request.form['password'] == result['password']:
+            user = User(result['id'], result['username'], result['banned'])
+
+            login_user(user)
+
+            return redirect('/feed')
 
         photo = request.files ['photo']
 
